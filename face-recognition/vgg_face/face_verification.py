ca3772dd6297eb06e2721bd4927a7145724298e4
Aniket Rai <aniketrai025@gmail.com>
2021-08-03 22:03:25 +1200

    Ready to port to jetson nano

--- face-recognition/vgg_face/face_verification.py
+++ face-recognition/vgg_face/face_verification.py
@@ -1,10 +1,10 @@
+import os
+import datetime
+
 from vgg_face.data_processing import preprocess
 from vgg_face.cosine_distance import find_cosine_distance
 
 def verify(img1, img2, model):
-  img1 = preprocess(img1)
-  img2 = preprocess(img2)
-
   img1_embedding = embedding(img1, model)
   img2_embedding = embedding(img2, model)
 
@@ -31,4 +31,16 @@ def compare(embedding_1, embeddeding_2):
     return True
   else:
     return False
+
+def init(model):
+  img_path = "/Users/aniketrai/Desktop/part-iv-project/face-recognition/images"
+  files = os.listdir(img_path)
+  faces = {}
+
+  for file in files:
+    timestamp = float(file[:-4])
+    file = f"{img_path}/{file}"
+    faces[timestamp] = embedding(file, model)
+
+  return faces
 
